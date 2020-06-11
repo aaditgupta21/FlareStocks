adbec69cab4a9b2815fbd9a86682276062dda499
Aadit Gupta <spacegiant21@gmail.com>
2020-06-10 23:55:20 -0700

    extracted data and half AI done

--- AI/AI.py
+++ AI/AI.py
@@ -14,3 +14,37 @@ with warnings.catch_warnings():
 
 
 
+os.chdir('../')
+sys.path.append(os.getcwd())
+
+tf.compat.v1.enable_eager_execution()
+
+def load_data(company):
+    data = pd.DataFrame()
+    if os.path.exists(f'data/stocks/{company}/{company}.pkl'):
+        data_ = pd.read_pickle(f'data/stocks/{company}/{comapny}.pkl')
+        data = pd.DataFrame(data_)
+        data.set_index('date', inplace=True)
+        data.drop(columns=['volume', 'adjClose', 'adjHigh', 'adjLow', 'adjOpen', 'adjVolume', 'divCash', 'splitFactor'], inplace=True)
+    return data
+
+def create_model(company, x, y, x_val, y_val): 
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(64, activation='linear'))
+    model.add(tf.keras.layers.Dense(32, activation='linear'))
+    model.add(tf.keras.layers.Dense(16, activation='linear'))
+    lower_rate = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=0, min_lr=0.00001, mode='min', verbose=1)
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(f'ai/models/{company}.h5', save_best_only=True, verbose=1)
+    early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=8, baseline=0.1)
+    board = tf.keras.callbacks.TensorBoard(log_dir='logs', histogram_freq='0', write_graph="True", write_images='False', update_Freq="epoch",profil_batch='2', embeddings_freq='20', embeddings_metadata=None)
+    model.compile(optimizer=tf.keras.optimizers.Adam(0.1), loss='mean_absolute_error')
+    model.fit(x=x, y=y, validation_data=(x_val, y_val), epochs=100, callbacks=[lower_rate, checkpoint, early, board], verbose=1)
+    return model
+
+def train():
+    start = time.time()
+    
+
+
+if __name__ == "__main__":
+    train()
--- data/extract_data.py
+++ data/extract_data.py
@@ -1,35 +1,67 @@
 import os
 import requests
-import sys
-import pickle
 import bs4
+import pickle
+import time
+print("Starting")
+start = time.time()
 
-def get_ticker():
-    req = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    table = bs4.BeautifulSoup(req.text, 'lxml').find('table', {'class': 'wikitable sortable'})
+def get_tickers():
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    table = bs4.BeautifulSoup(resp.text, 'lxml').find('table', {'class': 'wikitable sortable'})
 
-    tickers = []
+    symbols = []
     for row in table.findAll('tr')[1:]:
         symbol = row.findAll('td')[0].text
-        tickers.append(symbol.replace('\n', ''))
+        symbols.append(symbol.replace('\n', ''))
+
     with open("tickers.pkl", "wb") as f:
-        pickle.dump(tickers,f)
-    return tickers
+        pickle.dump(symbols, f)
+    return symbols
+
 
 def get_data():
     if not os.path.exists("tickers.pkl"):
-        tickers = get_ticker()
+        symbols = get_tickers()
     else:
-        with open("tickers.pkl", "wb") as f:
-            tickers = pickle.load(f)
+        with open("tickers.pkl", "rb") as f:
+            symbols = pickle.load(f)
 
     if not os.path.exists('stocks'):
         os.makedirs('stocks')
 
-    for ticker in tickers:
-        
+    for symbol in symbols:
+        if not os.path.exists('stocks/{}'.format(symbol)):
+            os.makedirs('stocks/{}'.format(symbol))
 
+        if not os.path.exists('stocks/{}/meta.csv'.format(symbol)):
+            try:
+                headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Token 31db9807b1b41a9e85229876c01472b6a4f263ed'
+                }
+                desc = requests.get("https://api.tiingo.com/tiingo/daily/{}?".format(symbol.replace('.', '-')), headers=headers)
+                with open('stocks/{}/desc.pkl'.format(symbol), "wb") as f:
+                    pickle.dump(desc.json(), f)
+                    print(symbol + ' dumping')
+            except Exception:
+                print(symbol + ' not found')
+
+        try:
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token 31db9807b1b41a9e85229876c01472b6a4f263ed'
+            }
+            eod = requests.get("https://api.tiingo.com/tiingo/daily/{}/prices?startDate=2019-01-02".format(symbol.replace('.', '-')), headers=headers)
+            with open('stocks/{}/{}.pkl'.format(symbol, symbol), "wb") as f:
+                pickle.dump(eod.json(), f)
+        except Exception:
+            print(symbol + ' not found')
 
 
 if __name__ == '__main__':
     get_data()
+    file_list = os.listdir("./stocks")
+    sorted(file_list)
+    print(f'Total time = {(time.time() - start) / 60} minutes')
+    print("Done")
